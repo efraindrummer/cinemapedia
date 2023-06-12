c643be1dd4390b0afba6889c10dc02be670e3241
Efrain May <efrain_may_75@hotmail.com>
2023-06-12 16:26:16 -0600

    go router official implement

--- lib/config/router/app_router.dart
+++ lib/config/router/app_router.dart
@@ -1,4 +1,5 @@
 
+import 'package:cinemapedia/presentation/views/views.dart';
 import 'package:go_router/go_router.dart';  
 import 'package:cinemapedia/presentation/screens/screens.dart';
 
@@ -6,10 +7,53 @@ import 'package:cinemapedia/presentation/screens/screens.dart';
 final appRouter = GoRouter(
   initialLocation: '/',
   routes: [
-    GoRoute(
+
+    ShellRoute(
+      builder: (context, state, child) {
+        return HomeScreen(childView: child);
+      },
+      routes: [
+        GoRoute(
+          path: '/',
+          builder: (context, state) {
+            return const HomeView();
+          },
+          routes: [
+            GoRoute(
+              path: 'movie/:id',
+              name: MovieScreen.name,
+              builder: (context, state) {
+                final movieId = state.pathParameters['id'] ?? 'no-id';
+                return MovieScreen(movieId: movieId);
+              },
+            ),
+            GoRoute(
+              path: 'person/:id',
+              name: ActorScreen.name,
+              builder: (context, state) {
+                final actorId = state.pathParameters['id'] ?? 'no-id';
+                return ActorScreen(actorId: actorId);
+              },
+            ),
+          ]
+        ),
+
+        GoRoute(
+          path: '/favorites',
+          builder: (context, state) {
+            return const FavoritesViews();
+          },
+        ),
+
+      ]
+    )
+
+
+    //Rutas padres /Hijo
+    /*GoRoute(
       path: '/',
       name: HomeScreen.name,
-      builder: (context, state) => const HomeScreen(),
+      builder: (context, state) => const HomeScreen(childView: HomeView(),),
       routes: [
         GoRoute(
           path: 'movie/:id',
@@ -28,6 +72,6 @@ final appRouter = GoRouter(
           },
         ),
       ]
-    ),
+    ),*/
   ]
 );
