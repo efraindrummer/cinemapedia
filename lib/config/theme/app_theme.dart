e11f01387765616127c79d6e890c86a7b3c7c134
Efrain May <efrain_may_75@hotmail.com>
2023-06-16 00:43:01 -0600

    ṕush

--- lib/config/theme/app_theme.dart
+++ lib/config/theme/app_theme.dart
@@ -5,6 +5,7 @@ class AppTheme {
 
   ThemeData getTheme() => ThemeData(
     useMaterial3: true,
-    colorSchemeSeed: const Color(0xFF2862F5)
+    colorSchemeSeed: const Color(0xFF2862F5),
+    //brightness: Brightness.light
   );
 }
